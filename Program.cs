42bac8ae736de819b9661f8190f10b95eb6cf3e5
tian-dutoit <tiandutoit.nz@gmail.com>
2018-05-01 15:18:58 +1200

    initial setup

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_intermediate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
