0c48a033e8728929b0973167d966dfdb9df9b778
tian-dutoit <tiandutoit.nz@gmail.com>
2018-05-04 17:12:10 +1200

    params example

--- Program.cs
+++ Program.cs
@@ -2,11 +2,22 @@
 
 namespace csharp_intermediate
 {
+    public class Calculator {
+        public int Add (params int[] numbers) {
+            var sum = 0;
+            foreach (var number in numbers)
+            {
+                sum += number;
+            }
+            return sum;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var calculator = new Calculator();
+            System.Console.WriteLine(calculator.Add(1, 2));
         }
     }
 }
